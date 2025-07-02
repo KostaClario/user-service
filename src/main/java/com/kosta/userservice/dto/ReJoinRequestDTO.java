7147e1de8636d534ae61c6a9f59ebe03772fa98d
taebin5824 <xoqls5824@gmail.com>
2025-07-02 23:53:28 +0900

    feat: 재가입(회원가입 분기처리) 요청 DTO

---
+++ src/main/java/com/kosta/userservice/dto/ReJoinRequestDTO.java
@@ -0,0 +1,23 @@
+package com.kosta.userservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ReJoinRequestDTO {
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotBlank(message = "이름은 필수입니다.")
+    private String name;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+
+    @NotBlank(message = "비밀번호 확인은 필수입니다")
+    private String confirmPassword;
+
+    @NotBlank(message = "전화번호는 필수입니다.")
+    private String phone;
+}
