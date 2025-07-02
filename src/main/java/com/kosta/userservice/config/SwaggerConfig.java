d9370aa97717f81cf28db4a181b8b23829bc9a0c
taebin5824 <xoqls5824@gmail.com>
2025-07-02 23:54:15 +0900

    feat: Swagger 설정 파일

---
+++ src/main/java/com/kosta/userservice/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.kosta.userservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "User API", version = "v1"),
+        servers = @Server(url = "http://localhost:8001"),
+        security = @SecurityRequirement(name = "bearerAuth") // 모든 API에 적용
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
+
