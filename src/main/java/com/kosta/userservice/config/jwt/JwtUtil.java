2a4440ec71f160b93b9e6d351c085f61656198f4
taebin5824 <xoqls5824@gmail.com>
2025-07-02 23:49:58 +0900

    feat: JWT 생성을 위한 JwtUtil 클래스 구현

---
+++ src/main/java/com/kosta/userservice/config/jwt/JwtUtil.java
@@ -0,0 +1,63 @@
+package com.kosta.userservice.config.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final JwtProperties jwtProperties;
+
+    public JwtUtil(final JwtProperties jwtProperties) {
+        this.jwtProperties = jwtProperties;
+    }
+
+    public String generateToken(String email, String picture){
+        return JWT.create()
+                .withSubject("User")
+                .withClaim("email", email)
+                .withClaim("picture", picture)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + jwtProperties.getExpiration()))
+                .sign(Algorithm.HMAC256(jwtProperties.getSecret()));
+    }
+
+    public String getEmailFromToken(String token){
+        try{
+            DecodedJWT jwt = JWT.require(Algorithm.HMAC256(jwtProperties.getSecret()))
+                    .build()
+                    .verify(token);
+            return jwt.getClaim("email").asString();
+        }catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+    public String getPictureFromToken(String token) {
+        try {
+            DecodedJWT jwt = JWT.require(Algorithm.HMAC256(jwtProperties.getSecret()))
+                    .build()
+                    .verify(token);
+            return jwt.getClaim("picture").asString();
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+    public boolean validateToken(String token){
+        try {
+            JWT.require(Algorithm.HMAC256(jwtProperties.getSecret()))
+                    .build()
+                    .verify(token);
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+}
