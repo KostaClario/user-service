1040daed697a965d18812b590a53e5ddcfc1ec63
taebin5824 <xoqls5824@gmail.com>
2025-07-02 23:48:04 +0900

    feat: 회원가입 응답 DTO

---
+++ src/main/java/com/kosta/userservice/dto/JoinResponseDTO.java
@@ -0,0 +1,13 @@
+package com.kosta.userservice.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JoinResponseDTO {
+
+    private String email;
+    private String name;
+}
