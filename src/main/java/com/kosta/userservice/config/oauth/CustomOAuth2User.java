255ae74ce1236304e65e733c9411ded3248f42c6
taebin5824 <xoqls5824@gmail.com>
2025-07-02 23:47:10 +0900

    feat: OAuth2 로그인 사용자 처리를 위한 CustomOAuth2User 및 서비스 구현

---
+++ src/main/java/com/kosta/userservice/config/oauth/CustomOAuth2User.java
@@ -0,0 +1,44 @@
+package com.kosta.userservice.config.oauth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+
+    private final String email;
+    private final String picture;
+
+    public CustomOAuth2User(String email, String picture) {
+        this.email = email;
+        this.picture = picture;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Map.of("email", email, "picture", picture);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+}
+
---
+++ src/main/java/com/kosta/userservice/config/oauth/CustomOAuth2UserService.java
@@ -0,0 +1,33 @@
+package com.kosta.userservice.config.oauth;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Slf4j
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        String email = (String) attributes.get("email");
+        String picture = (String) attributes.get("picture");
+
+        log.info("email = {}" , attributes.get("email"));
+        log.info("picture = {}" , attributes.get("picture"));
+
+        return new CustomOAuth2User(email, picture);
+    }
+}
