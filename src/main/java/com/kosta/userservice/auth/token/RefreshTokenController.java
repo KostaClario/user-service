557d02dccc2e34a1da591b8104c9a34921c319f7
taebin5824 <xoqls5824@gmail.com>
2025-07-08 00:56:05 +0900

    fix: 로그아웃 시 redis에서 토큰삭제 구현

--- src/main/java/com/kosta/userservice/auth/token/RefreshTokenController.java
+++ src/main/java/com/kosta/userservice/auth/token/RefreshTokenController.java
@@ -2,17 +2,13 @@ package com.kosta.userservice.auth.token;
 
 
 import com.kosta.userservice.auth.jwt.JwtUtil;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
+@Slf4j
 @RestController
 @RequestMapping("/token")
 public class RefreshTokenController {
@@ -37,6 +33,27 @@ public class RefreshTokenController {
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지않은 refresh token");
         }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestHeader("Authorization") String authHeader) {
+
+        log.info("Authorization 헤더 = {}", authHeader);
+
+        if (authHeader != null && !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Authorization 헤더가 유효하지 않습니다.");
+        }
+
+        String accessToken = authHeader.replace("Bearer ", "");
+        String userEmail = jwtUtil.getEmailFromToken(accessToken);
+
+        if (userEmail == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("토큰에서 이메일을 추출할 수 없습니다.");
+        }
+
+        refreshTokenService.deleteRefreshToken(userEmail);
+
+        return ResponseEntity.ok("로그아웃 성공: refresh token 삭제");
     }
 
 
