d16466cca5f40f902be0bf197044fe4dff41ae3c
taebin5824 <xoqls5824@gmail.com>
2025-07-10 00:40:57 +0900

    fix: 회원가입 시 토큰 발급, 비밀번호 재설정, 임의의 게이트웨이 테스트 추가

--- src/main/java/com/kosta/userservice/controller/MemberRestController.java
+++ src/main/java/com/kosta/userservice/controller/MemberRestController.java
@@ -1,10 +1,12 @@
 package com.kosta.userservice.controller;
 
+import com.kosta.userservice.auth.jwt.JwtUtil;
 import com.kosta.userservice.auth.oauth.CustomOAuth2User;
 import com.kosta.userservice.dto.JoinRequestDTO;
 import com.kosta.userservice.dto.JoinResponseDTO;
+import com.kosta.userservice.dto.ResetPasswordRequestDTO;
 import com.kosta.userservice.dto.UpdateProfileRequestDTO;
-import com.kosta.userservice.service.MemberService;
+import com.kosta.userservice.service.MemberServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +21,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class MemberRestController {
 
-    private final MemberService memberService;
+    private final MemberServiceImpl memberService;
+    private final JwtUtil jwtUtil;
 
-    public MemberRestController(MemberService memberService) {
+    public MemberRestController(MemberServiceImpl memberService, JwtUtil jwtUtil) {
         this.memberService = memberService;
+        this.jwtUtil = jwtUtil;
     }
+
+    @GetMapping("/member")
+    public ResponseEntity<?> me(@RequestHeader("X-User-Email") String email,
+                                @RequestHeader("X-User-Picture") String picture) {
+        return ResponseEntity.ok("email = " + email + ", picture = " + picture);
+    }
+
 
 
     @Operation(
@@ -31,9 +42,20 @@ public class MemberRestController {
             description = "이메일이 이미 존재하고 비활성화된 경우 재가입 처리합니다.")
     @PostMapping("/member")
     public ResponseEntity<JoinResponseDTO> join(@RequestBody @Validated JoinRequestDTO requestDTO) {
+
+        /**
+         * 나중에 bank-service에서 가져올거임 우선 회원가입 성공을 위해 넣어줌
+         *         requestDTO.setTotalAmount(0L);
+         *         requestDTO.setGoalAmount(0L);
+         * */
+
         memberService.createMember(requestDTO);
 
-        JoinResponseDTO responseData = new JoinResponseDTO(requestDTO.getEmail(), requestDTO.getName());
+        String email = requestDTO.getEmail();
+        String picture = requestDTO.getPicture();
+        String accessToken = jwtUtil.generateToken(email, picture);
+
+        JoinResponseDTO responseData = new JoinResponseDTO(email, requestDTO.getName(), accessToken);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseData);
     }
 
@@ -61,5 +83,18 @@ public class MemberRestController {
         String email = user.getEmail();
         memberService.removeMember(email);
         return ResponseEntity.status(HttpStatus.OK).body("회원 탈퇴 완료");
+    }
+
+    @Operation(summary = "비밀번호 재설정",
+            description = "현재 로그인된 사용자 비밀번호 재설정",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@AuthenticationPrincipal CustomOAuth2User user,
+                                           @RequestBody @Validated ResetPasswordRequestDTO request) {
+
+        memberService.resetPassword(user.getEmail(), request);
+
+        return ResponseEntity.status(HttpStatus.OK).body("비밀번호 재설정 완료");
+
     }
 }
