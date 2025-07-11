fb15c919f589fb580aea4c27ef550f784a7946df
taebin5824 <xoqls5824@gmail.com>
2025-07-11 18:44:27 +0900

    refactor: phone -> phoneNum 필드명 변경

--- src/main/java/com/kosta/userservice/dto/JoinRequestDTO.java
+++ src/main/java/com/kosta/userservice/dto/JoinRequestDTO.java
@@ -18,7 +18,7 @@ public class JoinRequestDTO {
     private String confirmPassword;
 
     @NotBlank(message = "전화번호는 필수입니다.")
-    private String phone;
+    private String phoneNum;
 
     @NotBlank(message = "이메일은 필수입니다.")
     private String email;
