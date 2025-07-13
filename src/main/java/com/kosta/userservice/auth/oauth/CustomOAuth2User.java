2d02edcae5bfa469c5b0bb7a8c7de9e0d17d5c6e
taebin5824 <xoqls5824@gmail.com>
2025-07-13 21:22:13 +0900

    refactor: provider, providerId 저장 로직 구현

--- src/main/java/com/kosta/userservice/auth/oauth/CustomOAuth2User.java
+++ src/main/java/com/kosta/userservice/auth/oauth/CustomOAuth2User.java
@@ -1,5 +1,7 @@
 package com.kosta.userservice.auth.oauth;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -7,20 +9,26 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-
+@Getter
+@EqualsAndHashCode
 public class CustomOAuth2User implements OAuth2User {
 
     private final String email;
     private final String picture;
+    private final String provider;
+    private final String providerId;
 
-    public CustomOAuth2User(String email, String picture) {
+    public CustomOAuth2User(String email, String picture, String provider, String providerId) {
         this.email = email;
         this.picture = picture;
+        this.provider = provider;
+        this.providerId = providerId;
     }
 
     @Override
     public Map<String, Object> getAttributes() {
-        return Map.of("email", email, "picture", picture);
+        return Map.of("email", email, "picture", picture,
+                "provider", provider, "providerId", providerId);
     }
 
     @Override
@@ -39,6 +47,14 @@ public class CustomOAuth2User implements OAuth2User {
 
     public String getPicture() {
         return picture;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public String getProviderId() {
+        return providerId;
     }
 }
 
