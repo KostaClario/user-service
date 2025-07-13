82a5366c6f2e76fc20871924990d1791488fe611
taebin5824 <xoqls5824@gmail.com>
2025-07-13 21:27:45 +0900

    refactor: bankapi 서비스간 인가처리

--- src/main/java/com/kosta/userservice/config/SecurityConfig.java
+++ src/main/java/com/kosta/userservice/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/internal/**").permitAll()
                         .requestMatchers(
                                 "/html/**",
                                 "/css/**",
