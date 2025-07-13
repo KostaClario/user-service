5e74215be5b3c49f8a25963b8d75b80ba28e5870
taebin5824 <xoqls5824@gmail.com>
2025-07-13 21:26:12 +0900

    refactor: ci, memberId로 찾기 구현

--- src/main/java/com/kosta/userservice/service/MemberService.java
+++ src/main/java/com/kosta/userservice/service/MemberService.java
@@ -4,9 +4,12 @@ import com.kosta.userservice.dto.JoinRequestDTO;
 import com.kosta.userservice.dto.ResetPasswordRequestDTO;
 import com.kosta.userservice.dto.UpdateProfileRequestDTO;
 
+import java.util.Optional;
+
 public interface MemberService {
     void createMember(JoinRequestDTO request);
     void updateProfile(String email, UpdateProfileRequestDTO request);
     void removeMember(String email);
     void resetPassword(String email, ResetPasswordRequestDTO request);
+    Optional<String> getMemberCiByMemberId(String memberId);
 }
