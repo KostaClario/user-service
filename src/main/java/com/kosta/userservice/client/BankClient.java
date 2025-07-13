df78242d950eb3f37e224808d4ccc14909c553cb
taebin5824 <xoqls5824@gmail.com>
2025-07-13 21:19:30 +0900

    feat: bankapi 거래내역 로직 추가

--- src/main/java/com/kosta/userservice/client/BankClient.java
+++ src/main/java/com/kosta/userservice/client/BankClient.java
@@ -1,15 +1,24 @@
 package com.kosta.userservice.client;
 
 
+import com.kosta.userservice.dto.BankApiDTO;
 import com.kosta.userservice.dto.BankUserInfoRequest;
 import com.kosta.userservice.dto.BankUserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient (name = "bank-service")
 public interface BankClient {
 
     @PostMapping("/api/bank/user-info")
     BankUserInfoResponse getUserInfo(@RequestBody BankUserInfoRequest request);
+
+
+    @GetMapping("/internal/transactions/{memberCi}")
+    List<BankApiDTO> getTransactions(@PathVariable("memberCi") String memberCi);
 }
