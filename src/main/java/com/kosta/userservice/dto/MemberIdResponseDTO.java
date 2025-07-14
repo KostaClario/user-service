15c31703ac18109b2f284034fc5dd89656a8fac7
taebin5824 <xoqls5824@gmail.com>
2025-07-15 01:48:48 +0900

    refactor: memberId 반환 DTO

---
+++ src/main/java/com/kosta/userservice/dto/MemberIdResponseDTO.java
@@ -0,0 +1,10 @@
+package com.kosta.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberIdResponseDTO {
+    private String memberId;
+}
